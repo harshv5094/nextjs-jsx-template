71c4aa7465dab0fabf93b0c2bb7b0a59f0d89559
Harsh Vyapari <harshv1741@gmail.com>
2023-07-15 14:19:14 +0530

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Harsh Vyapari</h1>
+      <h2>Nextjs template (JSX based)</h2>
+    </main>
+  )
+}
